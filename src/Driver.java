b24530c9d9bd6852bbd5e45cbf8d38f6b3dc3c7c
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-10-30 15:22:36 -0700

    Add menu

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,21 @@
 class Driver {
+  private static final String MENU = ""
+      + "+≡≡≡  Contact  App  ≡≡≡+\n"
+      + "| 1. List all Contacts |\n"
+      + "| 2. Add new Contact   |\n"
+      + "| 3. Remove Contact    |\n"
+      + "| 4. Update Contact    |\n"
+      + "| 5. Quit              |\n"
+      + "+======================+";
+  private static final String ASK_OPTION = "Enter your option: ";
+
   public static void main(String[] args) {
     ContactList contactList = new ContactList();
+    while(true) {
+      System.out.println(MENU);
+      String userInput = InputCollector.getUserInput(ASK_OPTION);
+      System.out.printf("userInput: %s\n", userInput);
+      break;
+    }
   }
 }
--- src/InputCollector.java
+++ src/InputCollector.java
@@ -1,2 +1,7 @@
 public class InputCollector {
+  public static String getUserInput(String prompt) {
+    System.out.print(prompt);
+    // TODO: implement
+    return "";
+  }
 }
