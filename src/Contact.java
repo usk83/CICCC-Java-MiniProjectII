88300ae9e33c74100c2d6e67547c73726ad5ccb8
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-11-04 12:17:25 -0800

    add printing method to Contact and ContactList

--- src/Contact.java
+++ src/Contact.java
@@ -1,3 +1,5 @@
+import java.lang.StringBuilder;
+
 class Contact {
   private String name;
   private String phoneNumberMobile;
@@ -20,5 +22,29 @@ class Contact {
     this.phoneNumberWork = phoneNumberWork;
     this.phoneNumberHome = phoneNumberHome;
     this.city = city;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append('<');
+    sb.append(name);
+    sb.append("> ");
+    sb.append("(mobile=");
+    sb.append(phoneNumberMobile);
+    if (!phoneNumberWork.isEmpty()) {
+      sb.append(", work=");
+      sb.append(phoneNumberWork);
+    }
+    if (!phoneNumberHome.isEmpty()) {
+      sb.append(", home=");
+      sb.append(phoneNumberHome);
+    }
+    if (!city.isEmpty()) {
+      sb.append(", city=");
+      sb.append(city);
+    }
+    sb.append(")");
+    return sb.toString();
   }
 }
--- src/ContactList.java
+++ src/ContactList.java
@@ -11,4 +11,15 @@ class ContactList {
   public void addContact(Contact newContact) {
     contacts.add(newContact);
   }
+
+  public void printAllContacts() {
+    int size = contacts.size();
+    if (size == 0) {
+      System.out.println("no entry.");
+      return;
+    }
+    for (int i = 0; i < size; i++) {
+      System.out.println(String.format("%d: %s", i, contacts.get(i)));
+    }
+  }
 }
