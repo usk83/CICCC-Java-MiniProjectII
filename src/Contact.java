5945d51477c6782d0342771afa74c24944af391f
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-11-04 12:03:39 -0800

    Implement Contact class

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,24 @@
 class Contact {
-  Contact() {
+  private String name;
+  private String phoneNumberMobile;
+  private String phoneNumberWork;
+  private String phoneNumberHome;
+  private String city;
+
+  Contact(String name, String phoneNumberMobile) {
+    this.name = name;
+    this.phoneNumberMobile = phoneNumberMobile;
+  }
+
+  Contact(
+      String name,
+      String phoneNumberMobile,
+      String phoneNumberWork,
+      String phoneNumberHome,
+      String city) {
+    this(name, phoneNumberMobile);
+    this.phoneNumberWork = phoneNumberWork;
+    this.phoneNumberHome = phoneNumberHome;
+    this.city = city;
   }
 }
